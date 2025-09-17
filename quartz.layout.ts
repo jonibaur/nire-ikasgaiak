6deafbc8fb22bbacb8cc5ca94b8e8eec8a75a6f0
jonibaur <jibarra@fpsanturtzilh.eus>
2025-09-17 12:42:35 +0200

    Quartz sync: Sep 17, 2025, 12:42 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -42,7 +42,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer({ title: "Menua ireki" }),
   ],
   right: [
-    Component.Graph(),
+    //Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
